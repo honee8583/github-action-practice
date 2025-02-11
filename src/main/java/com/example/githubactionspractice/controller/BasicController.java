5b335482e7afc37ecb7d7c3b706a0aee06e07288
leedahoon <ekgns0508@gmail.com>
2025-02-11 14:21:48 +0900

    new commit

--- src/main/java/com/example/githubactionspractice/controller/BasicController.java
+++ src/main/java/com/example/githubactionspractice/controller/BasicController.java
@@ -1,15 +1,17 @@
 package com.example.githubactionspractice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class BasicController {
 
     @GetMapping("/hello")
     public ResponseEntity<?> hello() {
-        // nothing
+        log.info("HELLO!");
         return ResponseEntity.ok().body("hello");
     }
 }
