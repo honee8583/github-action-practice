db018ecdb679a4fe5b85f764cae89bf97ba935d4
leedahoon <ekgns0508@gmail.com>
2025-02-13 16:21:18 +0900

    new commit

---
+++ src/main/java/com/example/githubactionspractice/entity/Member.java
@@ -0,0 +1,22 @@
+package com.example.githubactionspractice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
